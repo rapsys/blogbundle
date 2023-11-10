f3979ea4e65ed670ab5cf9c35af2842327b82d85
Raphaël Gertz <git@rapsys.eu>
2023-11-10 12:59:29 +0100

    Add group entity

---
+++ Entity/Group.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Rapsys BlogBundle package.
+ *
+ * (c) Raphaël Gertz <symfony@rapsys.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rapsys\BlogBundle\Entity;
+
+use Rapsys\UserBundle\Entity\Group as BaseGroup;
+
+class Group extends BaseGroup {
+}
